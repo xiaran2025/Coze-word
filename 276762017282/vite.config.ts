bb364f533d92ad1f13810ae7111dfb51e9b96009
xiaran2025 <xiaran2025@outlook.com>
2025-09-22 13:19:31 +0800

    Update vite.config.ts

--- 276762017282/vite.config.ts
+++ 276762017282/vite.config.ts
@@ -1,16 +1,17 @@
-/** WARNING: DON'T EDIT THIS FILE */
-/** WARNING: DON'T EDIT THIS FILE */
-/** WARNING: DON'T EDIT THIS FILE */
+/** WARNING: DON'T EDIT THIS FILE *//**警告：不要编辑这个文件*/
+/** WARNING: DON'T EDIT THIS FILE *//**警告：不要编辑这个文件*/
+/** WARNING: DON'T EDIT THIS FILE *//**警告：不要编辑这个文件*/
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import 从“vite”中导入{defineConfig}；{ defineConfig } from "vite";
+import 从“@vitejs/plugin-react”导入react；react from "@vitejs/plugin-react   反应";
+import 从“vite-tsconfig-paths”中导入tsconfigPaths；tsconfigPaths from "vite-tsconfig-paths";
 
-function getPlugins() {
-  const plugins = [react(), tsconfigPaths()];
-  return plugins;
+function    getPlugins() {getPlugins() {
+  const plugins = [react   反应(), tsconfigPaths()]；const plugins = [react(), tsconfigPaths()];
+     返回的插件;return plugins;
 }
 
-export default defineConfig({
-  plugins: getPlugins(),
+export 导出默认defineConfig({default defineConfig({
+  base: '/Coze - word/'
+     html页面:getPlugins (),plugins: getPlugins(),
 });
